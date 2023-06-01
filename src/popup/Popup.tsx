792b9efe609eadbc8b5a25af374393d89c2ee1d5
Ravi Kumar <kumar.ravi.vegan@gmail.com>
2023-06-01 21:14:53 +0530

    Initialize the application with create chrome extension boilerplate (react+typescript)

---
+++ src/popup/Popup.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './Popup.css'
+
+function App() {
+  return (
+    <div className='popup-container'>
+      <h2 className="title-text">Huzzle Autofill</h2>
+    </div>
+  )
+}
+
+export default App
