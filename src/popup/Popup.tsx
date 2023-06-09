26b9ee0740562243a280b71969177eb396499ab4
Ravi Kumar <kumar.ravi.vegan@gmail.com>
2023-06-09 10:34:13 +0530

    modify the popup layout like that of huzzle homepage.

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -5,50 +5,50 @@ const App = () => {
     <div className="popup-container">
       <h2 className="title-text">Huzzle Autofill</h2>
       <div className="top-buttons">
-        <button className="btn1">Autofill</button>
-        <button className="btn2">Reset</button>
-        <button className="btn3">Save</button>
-        <button className="btn4">Load</button>
+        <button className="btn">Autofill</button>
+        <button className="btn">Reset</button>
+        <button className="btn">Save</button>
+        <button className="btn">Load</button>
       </div>
       <div className="input-container">
-        <div>
-          <span>Name</span>
+        <div className="input-field">
+          <label>Name</label>
           <input></input>
         </div>
-        <div>
-          <span>Email</span>
+        <div className="input-field">
+          <label>Email</label>
           <input></input>
         </div>
-        <div>
-          <span>Telephone Number</span>
+        <div className="input-field">
+          <label>Telephone Number</label>
           <input></input>
         </div>
-        <div>
-          <span>Address</span>
+        <div className="input-field">
+          <label>Address</label>
           <input></input>
         </div>
-        <div>
-          <span>City</span>
+        <div className="input-field">
+          <label>City</label>
           <input></input>
         </div>
-        <div>
-          <span>Postcode</span>
+        <div className="input-field">
+          <label>Postcode</label>
           <input></input>
         </div>
-        <div>
-          <span>Card Number</span>
+        <div className="input-field">
+          <label>Card Number</label>
           <input></input>
         </div>
-        <div>
-          <span>Expiry Month</span>
+        <div className="input-field">
+          <label>Expiry Month</label>
           <input></input>
         </div>
-        <div>
-          <span>Expiry Year</span>
+        <div className="input-field">
+          <label>Expiry Year</label>
           <input></input>
         </div>
-        <div>
-          <span>CVV</span>
+        <div className="input-field">
+          <label>CVV</label>
           <input></input>
         </div>
       </div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,23 @@
+interface Manifest {
+  name: string
+  description: string
+  version: string
+  manifest_version: number
+  // Rest of the properties...
+  // You might need to specify all properties that are present in your actual manifest
+}
+
 import { defineConfig } from 'vite'
 import { crx } from '@crxjs/vite-plugin'
 import react from '@vitejs/plugin-react'
-import zipPack from 'vite-plugin-zip-pack';
+import zipPack from 'vite-plugin-zip-pack'
 
-import manifest from './src/manifest'
+// Import manifest and assert the type as Manifest
+import manifestData from './src/manifest'
+const manifest = manifestData as Manifest
+
 //@ts-ignore
-import {config} from './src/read_pages_folder'
+import { config } from './src/read_pages_folder'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -21,11 +33,17 @@ export default defineConfig(({ mode }) => {
       },
     },
 
-    plugins: [crx({ manifest }), react(),zipPack({
+    plugins: [
+      crx({ manifest }),
+      react(),
+      zipPack({
         outDir: `package`,
         inDir: 'build',
         // @ts-ignore
-        outFileName: `${manifest.short_name ?? manifest.name.replaceAll(" ", "-")}-extension-v${manifest.version}.zip`,
-      }),],
+        outFileName: `${manifest.short_name ?? manifest.name.replaceAll(' ', '-')}-extension-v${
+          manifest.version
+        }.zip`,
+      }),
+    ],
   }
 })
