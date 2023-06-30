8229c8b40cf26fbb9edf7c91d62154eac2369027
Ravi Kumar <kumar.ravi.vegan@gmail.com>
2023-06-30 22:40:45 +0530

    update the Options page for storing only desired fields and change layout of the options page.

--- src/options/Options.tsx
+++ src/options/Options.tsx
@@ -1,11 +1,36 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import './Options.css'
 
+interface UserData {
+  [key: string]: any
+}
+
 export default function Options() {
-  const [email, setEmail] = useState('')
-  const [otp, setOtp] = useState('')
-  const [userData, setUserData] = useState({})
+  const [email, setEmail] = useState<string>('')
+  const [otp, setOtp] = useState<string>('')
+  const [userData, setUserData] = useState<UserData>({})
+
+  const keysOfInterest = [
+    'avatar_url',
+    'bio',
+    'date_of_birth',
+    'email',
+    'first_name',
+    'last_name',
+    'linkedin_url',
+    'name',
+    'remote_opportunity',
+    'resume_url',
+  ]
+
+  useEffect(() => {
+    chrome.storage.local.get(['userData'], function (result) {
+      if (result.userData) {
+        setUserData(result.userData)
+      }
+    })
+  }, [])
 
   const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) =>
     setEmail(event.target.value)
@@ -22,12 +47,8 @@ export default function Options() {
     }
     axios
       .post('https://api.huzzle.app/api/v1/candidate/login', data)
-      .then((response) => {
-        console.log(response.data) // Display the response from your API
-      })
-      .catch((error) => {
-        console.error('An error occurred:', error)
-      })
+      .then((response) => console.log(response.data))
+      .catch((error) => console.error('An error occurred:', error))
   }
 
   const verifyOTP = (event: React.FormEvent<HTMLFormElement>) => {
@@ -40,17 +61,18 @@ export default function Options() {
         },
       })
       .then((response) => {
-        console.log(response.data) // Display the response from your API
-        setUserData(response.data) // Update state with user data
+        console.log(response.data)
+        setUserData(response.data.data.attributes)
+        chrome.storage.local.set({ userData: response.data.data.attributes }, function () {
+          console.log('User data is stored in local storage.')
+        })
       })
-      .catch((error) => {
-        console.error('An error occurred:', error)
-      })
+      .catch((error) => console.error('An error occurred:', error))
   }
 
   return (
     <div className="container">
-      <div className="form-container">
+      <div className="form-container-left">
         <h2>Huzzle SignIn</h2>
         <form onSubmit={handleSubmit} className="left-form">
           <label>
@@ -69,14 +91,23 @@ export default function Options() {
         </form>
       </div>
 
-      <div className="form-container">
+      <div className="form-container-right">
         {userData && Object.keys(userData).length > 0 && (
           <div className="right-form">
-            {Object.keys(userData).map((key) => (
-              <p key={key}>
-                {key}: {userData[key]}
-              </p>
-            ))}
+            {keysOfInterest.map((key) =>
+              userData[key] ? (
+                <p key={key}>
+                  {' '}
+                  {key}: {JSON.stringify(userData[key])}
+                </p>
+              ) : null,
+            )}
+            {userData.degree && userData.degree.data && userData.degree.data.attributes && (
+              <p>degree.name: {userData.degree.data.attributes.name}</p>
+            )}
+            {userData.subject && userData.subject.data && userData.subject.data.attributes && (
+              <p>subject.name: {userData.subject.data.attributes.name}</p>
+            )}
           </div>
         )}
       </div>
